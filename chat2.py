ccba2795914bd46e247dc3c015beb7798db20e48
woozhscode <166362397+woozhscode@users.noreply.github.com>
2024-07-26 13:50:29 +0900

    Update chat2.py

--- chat2.py
+++ chat2.py
@@ -1,35 +1,43 @@
 import streamlit as st
 from openai import OpenAI
 import time
+from langchain.memory import StreamlitChatMessageHistory
+
+st.set_page_config(
+    page_title="MMR DEMO",
+    page_icon=":books:"
+    )
 
 with st.sidebar:
-    openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
-    uploaded_file = st.file_uploader("Choose a file")
+    # openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
+    uploaded_file = st.file_uploader("🔗Choose a file👋")
     #"[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
     #"[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
     #"[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
 
 i = 0.5
 st.image("C:/Users/SE922PU/OneDrive - EY/Desktop/zzz/고객사로고..png", width=81)
-st.title("💬 Multi-Modal RAG Chatbot")
+st.title("🔎*Multi-Modal RAG* :red[*Chatbot*]🤖")
+
 col1, col2, col3 = st.columns([4, 0.5, 0.1])
 with col1:
     st.write("\n")
     st.write("\n")
 with col2:
-    st.caption("🚀🚀🚀   created by")
+    st.caption("😊😊😊  *created by*")
 with col3:
     st.image("C:/Users/SE922PU/OneDrive - EY/Desktop/zzz/이와이로고.png", width=36)
 
-
 if "messages" not in st.session_state: # 챗봇 맨처음 상황
-    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+    st.session_state["messages"] = [{"role": "assistant", "content": "무엇을 도와드릴까요?"}]
 
 for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"]) # 이전기록 화면 출력
     i += 0.5
-  
-if prompt := st.chat_input("Type a message👋"):
+
+history = StreamlitChatMessageHistory(key="chat_massages")
+
+if prompt := st.chat_input("Type a message 💬"):
     if uploaded_file is not None:
         st.image(uploaded_file)
     st.session_state.messages.append({"role": "user", "content": prompt})
@@ -40,3 +48,18 @@ if prompt := st.chat_input("Type a message👋"):
         msg = f"{i}" #response.choices[0].message.content
     st.session_state.messages.append({"role": "assistant", "content": msg})
     st.chat_message("assistant").write(msg) # 화면 출력
+    st.success('✔️')
+
+# if not openai_api_key:
+#     if uploaded_file is not None:
+#         st.image(uploaded_file)
+#     st.info("Please add your OpenAI API key to continue.")
+#     st.stop()
+
+# client = OpenAI(api_key=openai_api_key)
+# st.session_state.messages.append({"role": "user", "content": prompt})
+# st.chat_message("user").write(prompt)
+# response = client.chat.completions.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
+# msg = response.choices[0].message.content
+# st.session_state.messages.append({"role": "assistant", "content": msg})
+# st.chat_message("assistant").write(msg)
